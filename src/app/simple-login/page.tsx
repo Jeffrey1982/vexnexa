abf8c120dab5bd2d954c24df7237cce81e838af4
Jeffrey1982 <jeffrey.aay@gmail.com>
2025-09-24 19:16:52 +0200

    fix: escape apostrophe in simple-login page to resolve ESLint error
    Changed "Don't" to "Don&apos;t" to fix react/no-unescaped-entities error
    Build should now complete successfully.

--- src/app/simple-login/page.tsx
+++ src/app/simple-login/page.tsx
@@ -108,7 +108,7 @@ export default function SimpleLoginPage() {
           </form>
 
           <div className="mt-6 text-center text-sm">
-            <span className="text-gray-600">Don't have an account? </span>
+            <span className="text-gray-600">Don&apos;t have an account? </span>
             <Link href="/simple-register" className="text-blue-600 hover:underline">
               Sign up
             </Link>
